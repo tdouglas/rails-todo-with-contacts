cfdf7289c0374fb91accfecb33aef64ff63e018f
tdouglas <tdouglas@seamlessTL.com>
2013-07-08 19:40:13 -0400

    can display all tasks

--- rails_to_do/app/controllers/todos_controller.rb
+++ rails_to_do/app/controllers/todos_controller.rb
@@ -1,11 +1,23 @@
 class TodosController < ApplicationController
   def index
-    @todos = Todos.all
+    @tasks = Task.all
   end
 
   def new
   end
 
   def create
+    task = Task.new
+    task.name = params[:name]
+    task.description = params[:description]
+    task.due_date = params[:due_date]
+    task.urgent = params[:urgent]
+    if urgency == "on"
+      urgency = true
+    else
+      urgency = false
+    end
+    task.save
+    redirect_to '/todos'
   end
 end
--- rails_to_do/db/migrate/20130708213614_tasks.rb
+++ rails_to_do/db/migrate/20130708213614_tasks.rb
@@ -2,8 +2,10 @@ class Tasks < ActiveRecord::Migration
   def up
     create_table :tasks do |t|
       t.string :name
+      t.string :description
       t.date :due_date
       t.boolean :urgent
+    end
   end
 
   def down
